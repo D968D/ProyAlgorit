2cdcf07b3ea3949b8f0a5376fb6b599d2268d109
Samantha <Ssarmientopalomino@gmail.com>
2026-09-04 22:31:38 -0500

    Implement Plato class with attributes and methods
    Added attributes and methods for Plato class.

--- ProyAlgorit/src/main/java/model/Plato.java
+++ ProyAlgorit/src/main/java/model/Plato.java
@@ -1,4 +1,41 @@
 package model;
 
 public class Plato {
+    private String codigoPlato;
+    private String nombre;
+    private double precio;
+    private String categoria;
+    private int stock;
+
+    public Plato() {}
+
+    public Plato(String codigoPlato, String nombre, double precio, String categoria, int stock) {
+        this.codigoPlato = codigoPlato;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+        this.stock = stock;
+    }
+
+    // Getters y Setters
+    public String getCodigoPlato() { return codigoPlato; }
+    public void setCodigoPlato(String codigoPlato) { this.codigoPlato = codigoPlato; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public double getPrecio() { return precio; }
+    public void setPrecio(double precio) { this.precio = precio; }
+
+    public String getCategoria() { return categoria; }
+    public void setCategoria(String categoria) { this.categoria = categoria; }
+
+    public int getStock() { return stock; }
+    public void setStock(int stock) { this.stock = stock; }
+
+    @Override
+    public String toString() {
+        return codigoPlato + " - " + nombre + " S/ " + precio;
+    }
+}
 }
