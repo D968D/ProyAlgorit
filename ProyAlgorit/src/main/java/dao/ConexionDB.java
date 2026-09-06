222b81593d658fbec8301541133b4736d19f9697
Diego Gabriel Gutierrez Sotelo <281473403+D968D@users.noreply.github.com>
2026-09-05 23:30:51 -0500

    05/09/2026
    Co-authored-by: Samantha <Ssarmientopalomino@gmail.com>

--- ProyAlgorit/src/main/java/dao/ConexionDB.java
+++ ProyAlgorit/src/main/java/dao/ConexionDB.java
@@ -1,14 +1,11 @@
 package dao;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionDB {
 
-    
- 
-    private static final String URL = "jdbc:postgresql://localhost:5432/cordonrosadb";
+    private static final String URL = "jdbc:postgresql://localhost:5432/CordonRosaDB";
     private static final String USER = "postgres";
     private static final String PASS = "admin";
 
